741459512dda6a8f728ec774768351a4aa7fb7ce
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-07-02 09:17:27 +0900

    いろいろ登録

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,15 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { initializeOutlineTreeView } from "./feature/outline-tree-view";
+import { initializeIconsWebView } from "./feature/svg-icon-viewer";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(...initializeOutlineTreeView(context));
+
+  const iconsWebView = await initializeIconsWebView(context);
+  context.subscriptions.push(...iconsWebView);
 }
 
 // This method is called when your extension is deactivated
